7505555bb13fa130861bab700b41a011ebbff315
maikelsouza <maikel.souza@gmail.com>
2020-01-08 23:23:31 -0200

    mais um commit

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -33,7 +33,7 @@ export class ListPage implements OnInit {
   ngOnInit() {
   }
   // add back when alpha.4 is out
-  // navigate(item) {
+  // navigate(item) { 
   //   this.router.navigate(['/list', JSON.stringify(item)]);
   // }
 }
