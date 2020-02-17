85938b31cc087af1f688ad8b83b42927dd65f335
maikelsouza <maikel.souza@gmail.com>
2020-02-17 00:12:55 -0300

    Limpeza Arquivo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,11 @@ import { LoginComponent } from './login/login/login.component';
 
 
 const routes: Routes = [
-  // { path: 'list', loadChildren: './list/list.module#ListPageModule'},
-  // tslint:disable-next-line:max-line-length
   { path: 'estabelecimentos', loadChildren: './estabelecimentos/estabelecimentos.module#EstabelecimentosModule'},  
   { path: 'usuarios', loadChildren: './usuarios/usuarios.module#UsuariosModule'},    
-  { path: 'login', component: LoginComponent},  
-  
+  { path: 'login', component: LoginComponent},    
   { path: 'cartaoFidelidade', loadChildren: './cartoesFidelidade/cartoesFidelidade.module#CartoesFidelidadeModule'},  
-  { path: 'programaFidelidade', loadChildren: './programasFidelidade/programasFidelidade.module#ProgramasFidelidadeModule'},  
-  //{ path: 'lista-estabelecimento', loadChildren: './estabelecimentos/lista-estabelecimento/lista-estabelecimento.module#ListaEstabelecimentoPageModule' },
+  { path: 'programaFidelidade', loadChildren: './programasFidelidade/programasFidelidade.module#ProgramasFidelidadeModule'},    
   { path: 'home', loadChildren: './home/home.module#HomePageModule'},  
   { path: '', redirectTo: 'login', pathMatch: 'full'},
 ];
