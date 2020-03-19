07c5857f3bd5e2c001a6955701e823e946b5d5e0
maikelsouza <maikel.souza@gmail.com>
2020-03-19 18:19:58 -0300

    Arquivo para registrar os pontos do prog. Fidel

---
+++ src/app/pontuarClientes/shared/models/pontos-clientes-programa-fidelidade.spec.ts
@@ -0,0 +1,7 @@
+import { PontosClientesProgramaFidelidade } from './pontos-clientes-programa-fidelidade';
+
+describe('PontosClientesProgramaFidelidade', () => {
+  it('should create an instance', () => {
+    expect(new PontosClientesProgramaFidelidade()).toBeTruthy();
+  });
+});
---
+++ src/app/pontuarClientes/shared/models/pontos-clientes-programa-fidelidade.ts
@@ -0,0 +1,9 @@
+export class PontosClientesProgramaFidelidade {
+
+    id: number;
+    pontos: number;    
+    dataPontuacao: Date; 
+    ativo: boolean;
+    usuarioId: number;
+    programaFidelidadeId: number;
+}
