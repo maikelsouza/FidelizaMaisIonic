82749b9d9f89e857b19c9771ed3a18488c5caf5f
maikelsouza <maikel.souza@gmail.com>
2020-06-30 01:03:14 -0300

    Limpando arquivo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+
 import { LoginService } from './login/shared/services/login.service';
 import { Usuario } from './usuarios/shared/models/usuario';
 
@@ -18,48 +18,7 @@ export class AppComponent {
   mostrarMenu: boolean = false;
   usuarioLogado: Usuario;
 
-  public appPages = [
-    { // 0
-      title: 'Meu Perfil',
-      url: '/usuarios/meuPerfil',
-      icon: 'home',
-      exibir: 'true'
-    },
-    
-    { // 1
-      title: 'Estabelecimento',
-      url: '/estabelecimentos',
-      icon: 'home',
-      exibir: 'true'      
-    },
-
-    { // 2
-      title: 'Usuário',
-      url: '/usuarios',
-      icon: 'home',
-      exibir: 'false'           
-    },
-   /* {
-      title: 'Login',
-      url: '/login',
-      icon: 'home',
-      exibir: 'false'     
-    },*/
-
-    { // 3
-      title: 'Cartão Fidelidade',
-      url: '/cartaoFidelidade',
-      icon: 'home',
-      exibir: 'false'     
-    },
-
-    { // 4
-      title: 'Programa Fidelidade',
-      url: '/programaFidelidade',
-      icon: 'home',     
-      exibir: 'false'     
-    }
-  ];
+  
 
   constructor(
     private platform: Platform,
