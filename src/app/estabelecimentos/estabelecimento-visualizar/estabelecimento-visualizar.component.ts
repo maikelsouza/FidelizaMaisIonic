0a897a8e64cb5feed55ced91ab1329da3d116342
maikelsouza <maikel.souza@gmail.com>
2020-08-28 00:53:19 -0300

    Incluído ter botão aderir ou não prog. fidelidade

--- src/app/estabelecimentos/estabelecimento-visualizar/estabelecimento-visualizar.component.ts
+++ src/app/estabelecimentos/estabelecimento-visualizar/estabelecimento-visualizar.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { EstabelecimentoService } from '../shared/services/estabelecimento.service';
-import { Estabelecimento } from '../shared/models/estabelecimento';
+import { Router } from '@angular/router';
+
 import { EstabelecimentoVO } from '../shared/vos/estabelecimento-vo';
+import { ClienteEstabelecimento } from '../shared/models/cliente-estabelecimento';
+import { ClienteEstabelecimentoService } from '../shared/services/cliente-estabelecimento.service';
+import { AutenticadorService } from 'src/app/common/service/autenticador.service';
+import { Usuario } from 'src/app/usuarios/shared/models/usuario';
+import { AlertaService } from 'src/app/common/service/alerta.service';
+import { EstabelecimentoService } from '../shared/services/estabelecimento.service';
 
 @Component({
   selector: 'app-estabelecimento-visualizar',
@@ -15,17 +18,54 @@ export class EstabelecimentoVisualizarComponent implements OnInit {
 
     
   estabelecimentoVO : EstabelecimentoVO;
+  usuarioLogado: Usuario;
   
   constructor(    
-    private router: Router
+    private router: Router,
+    private clienteEstabelecimentoService: ClienteEstabelecimentoService,  
+    private alertSrv: AlertaService,
+    private estabelecimentoService: EstabelecimentoService,
   ) {
     const nav = this.router.getCurrentNavigation();
     this.estabelecimentoVO = nav.extras.state.estabelecimentoVO; 
    }
 
   ngOnInit() {
-   
-   
+    this.usuarioLogado = AutenticadorService.UsuarioLogado;      
   } 
+
+  async associarClienteEstabelecimento(estabelecimentoId: number){
+    try { 
+      let clienteEstabelecimento : ClienteEstabelecimento = new ClienteEstabelecimento(); 
+      clienteEstabelecimento.dataCriacao = new Date();
+      clienteEstabelecimento.estabelecimentoId = estabelecimentoId;
+      clienteEstabelecimento.usuarioId = this.usuarioLogado[0].id;
+      let resultado = await this.clienteEstabelecimentoService.salvar(clienteEstabelecimento);
+      if (resultado.success){
+        this.alertSrv.toast('Cliente associado com sucesso!');        
+        await this.estabelecimentoService.notificarListaEstabelecimento();
+        this.router.navigate(['/estabelecimento/lista'],{ queryParams: { tipoUsuario: this.usuarioLogado[0].GrupoUsuario.nome } });             
+      }
+    } catch (error) {
+        console.log('Erro ao associar um cliente a um estabelecimento', error);    
+    }
+  }
+
+  async desassociarClienteEstabelecimento(estabelecimentoId: number) {
+    try {
+      let clienteEstabelecimento : ClienteEstabelecimento = new ClienteEstabelecimento(); 
+      clienteEstabelecimento.dataCriacao = new Date();
+      clienteEstabelecimento.estabelecimentoId = estabelecimentoId;
+      clienteEstabelecimento.usuarioId = this.usuarioLogado[0].id;      
+      let resultado = await this.clienteEstabelecimentoService.deletePorUsuarioEEstabelecimento(this.usuarioLogado[0].id, estabelecimentoId);
+      if (resultado.success) {
+        this.alertSrv.toast('Cliente desassociar com sucesso!');
+        await this.estabelecimentoService.notificarListaEstabelecimento();
+        this.router.navigate(['/estabelecimento/lista'],{ queryParams: { tipoUsuario: this.usuarioLogado[0].GrupoUsuario.nome } });                                                    
+      }
+    } catch (error) {
+      console.log('Erro ao desassociar um cliente a um estabelecimento', error);
+    }
+  }
 
 }
