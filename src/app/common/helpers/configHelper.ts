304ebf1e14a8e9cc26f0de3f7715217ddf3508ff
maikelsouza <maikel.souza@gmail.com>
2020-09-29 23:46:22 -0300

    Nova funcionalidade manter-me conectado

--- src/app/common/helpers/configHelper.ts
+++ src/app/common/helpers/configHelper.ts
@@ -1,12 +1,14 @@
 export class ConfigHelper {
-     //public static Url: string = '/api/' 
+   //  public static Url: string = '/api/' 
     
-   // public static Url: string = 'http://localhost:3000/api/';
+    //public static Url: string = 'http://localhost:3000/api/';
     public static Url: string = 'http://appfidelizamais-com-br.umbler.net/api/'
                                           
 
     public static storageKeys = {
         token: 'cartaoFidelidade.token',
-        usuario: 'cartaoFidelidade.usuario'        
+        usuario: 'cartaoFidelidade.usuario',        
+        usuarioEmail: 'fidelizaMais.usuario.email',        
+        usuarioSenha: 'fidelizaMais.usuario.senha'
     }
 }
