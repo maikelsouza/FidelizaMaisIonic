40757506448cf0587d58a289307302d435ad2adf
maikelsouza <maikel.souza@gmail.com>
2020-10-25 08:07:45 -0300

    Correção erro  ClearTextTraffic android 9+

--- src/app/common/helpers/configHelper.ts
+++ src/app/common/helpers/configHelper.ts
@@ -1,8 +1,8 @@
 export class ConfigHelper {
    //  public static Url: string = '/api/' 
     
-    //public static Url: string = 'http://localhost:3000/api/';
-    public static Url: string = 'http://appfidelizamais-com-br.umbler.net/api/'
+   //public static Url: string = 'http://localhost:3000/api/';
+    public static Url: string = 'https://appfidelizamais-com-br.umbler.net/api/'
                                           
 
     public static storageKeys = {
