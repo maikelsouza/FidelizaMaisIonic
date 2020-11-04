55cafe3645b29880a73a7c64217d77a867229913
maikelsouza <maikel.souza@gmail.com>
2020-11-03 22:45:17 -0300

    Criação nova tela lista dos meus clientes

---
+++ src/app/pages/meus-clientes/meus-clientes-lista/meus-clientes-lista.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meus-clientes-lista',
+  templateUrl: './meus-clientes-lista.page.html',
+  styleUrls: ['./meus-clientes-lista.page.scss'],
+})
+export class MeusClientesListaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
